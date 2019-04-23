bb86372dc33e40785fed5096544cfd819f7d4242
xiaochenxc <xiaocheq@gmail.com>
2019-04-22 17:52:55 -0700

    Updates

---
+++ precache-manifest.791c3b5496fe5db04e53e4b0e087a13d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d58526874dc2febbf09f6e896b1ce9ad",
+    "url": "/xiaochenxc/MyFirstReactApp/index.html"
+  },
+  {
+    "revision": "4018b04164cad293e4a0",
+    "url": "/xiaochenxc/MyFirstReactApp/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/xiaochenxc/MyFirstReactApp/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "4018b04164cad293e4a0",
+    "url": "/xiaochenxc/MyFirstReactApp/static/js/main.ea39b35a.chunk.js"
+  },
+  {
+    "revision": "e8e704fddf72d8e64eac",
+    "url": "/xiaochenxc/MyFirstReactApp/static/js/runtime~main.5c5db74f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/xiaochenxc/MyFirstReactApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
