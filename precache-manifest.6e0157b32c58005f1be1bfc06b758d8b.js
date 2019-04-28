1928dff23990ddd4cc06aa9ad8ce59f455083b1f
xiaochenxc <xiaocheq@gmail.com>
2019-04-28 09:47:11 -0700

    Updates

---
+++ precache-manifest.6e0157b32c58005f1be1bfc06b758d8b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aed7a1c1c9fbbe074555ed578ab5c501",
+    "url": "/MyFirstReactApp/index.html"
+  },
+  {
+    "revision": "7171242a2d1e81091380",
+    "url": "/MyFirstReactApp/static/js/2.db733784.chunk.js"
+  },
+  {
+    "revision": "c77c57d8d6c5d536dbce",
+    "url": "/MyFirstReactApp/static/js/main.3d6538a7.chunk.js"
+  },
+  {
+    "revision": "823ac343f16c0646ad95",
+    "url": "/MyFirstReactApp/static/js/runtime~main.ad9edcd2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/MyFirstReactApp/precache-manifest.b696a0f8ec8296ab67995cc44202dde0.js"
+  "/MyFirstReactApp/precache-manifest.6e0157b32c58005f1be1bfc06b758d8b.js"
 );
 
 self.addEventListener('message', (event) => {
